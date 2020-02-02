cb67e636f0c67043ffbd8214002731fa4050b8f6
devleni <[devleni@fastmail.com]>
2020-02-02 09:37:08 +0100

    add remove posts functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import { http } from "./http";
 import { ui } from "./ui";
 
+// Get posts on DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
+
+// Listen for add post
+document.querySelector(".post-submit").addEventListener("click", addPost);
+
+// Listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
 // Get Posts
 function getPosts() {
   http.get("http://localhost:3000/posts")
@@ -28,9 +37,18 @@ function addPost() {
     .catch(err => console.log(err));
 }
 
-
-// Get posts on DOM load
-document.addEventListener("DOMContentLoaded", getPosts);
-
-// Listen for add post
-document.querySelector(".post-submit").addEventListener("click", addPost);
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm("Are you sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert("Post Removed", "alert alert-success");
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  } 
+}
